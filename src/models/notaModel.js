7612e732e7f48bebbab33e1a19586cce27805549
Luisvgf30 <luisvgf30@gmail.com>
2024-04-22 18:40:04 +0200

    Un nuevo amanecer

--- src/models/notaModel.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-
-var notaSchema = new Schema({
-    descripcion_nota : String,
-    fecha_nota : Date
-});
-
-module.exports = mongoose.model('Nota', notaSchema);
